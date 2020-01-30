bf12f22eca4f260bc26e1dfc0f80e60934a99952
czz <czzonet@163.com>
2020-01-30 14:38:22 +0800

    A layout

---
+++ src/layout/main.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="main-layout">
+    <div class="header">
+      <bar-header></bar-header>
+    </div>
+    <div class="body">
+      <div class="side">
+        <bar-side></bar-side>
+      </div>
+      <div class="content"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+/** component */
+import BarHeader from "../menu/header/index";
+import BarSide from "../menu/side/index";
+
+export default {
+  name: "MainLayout",
+  components: { BarHeader, BarSide }
+};
+</script>
+
+<style lang="scss" scoped>
+.main-layout {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+
+  height: 100%;
+
+  .header {
+    flex-basis: 100px;
+  }
+
+  .body {
+    flex: 1;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: stretch;
+
+    height: 100%;
+
+    .side {
+      flex-basis: 200px;
+    }
+
+    .content {
+      flex: 1;
+    }
+  }
+}
+</style>
